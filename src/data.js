06d117abec81985ca91dbb8bb2a79f4a9b7b9074
Mederic Carriat <carriatmederic@gmail.com>
2021-08-29 19:23:07 +0200

    Finish filling descriptions

--- src/data.js
+++ src/data.js
@@ -12,21 +12,8 @@ const experienceData = [
     startDate: new Date(2017, 9),
     endDate: new Date(2020, 7),
     description: [
-      'Courses in programming, mathematics, algorithmics, computer architecture.',
-      'Tags : OCaml, Python, C, C++, Unix'
-    ],
-    color: '#0000ff',
-    position: 'center'
-  },
-  {
-    company: 'EPITA',
-    logo: epita,
-    title: 'Master in Image Processing and Computer Graphics',
-    startDate: new Date(2020, 9, 1),
-    endDate: new Date(2022, 7),
-    description: [
-      'Courses in ',
-      'Tags: C++, Python, OpenGL'
+      <p>Courses in programming, mathematics, algorithmics, computer architecture.</p>,
+      <p>Tags : <b>OCaml, Python, C, C++, Unix</b></p>
     ],
     color: '#0000ff',
     position: 'center'
@@ -38,7 +25,7 @@ const experienceData = [
     startDate: new Date(2018, 7, 1),
     endDate: new Date(2018, 8, 31),
     description: [
-      'Renovation of Latresnes\' church with 10 other international students.'
+      <p>Renovation of Latresnes' church with 10 other international students.</p>
     ],
     color: '#ffaa00',
     position: 'bottom'
@@ -50,8 +37,8 @@ const experienceData = [
     startDate: new Date(2019, 1, 1),
     endDate: new Date(2019, 5),
     description: [
-      'Courses in Machine Learning, signal compression and Software Engineering.',
-      '3rd out of 105 teams at the university\'s Kaggle competition.'
+      <p>Courses in Machine Learning, signal compression and Software Engineering.</p>,
+      <p><b>3rd out of 105 teams</b> at the university's <u><a href='https://www.kaggle.com/c/robotsurface/overview'>Kaggle competition</a></u>.</p>
     ],
     color: '#aa00ff',
     position: 'top'
@@ -63,12 +50,25 @@ const experienceData = [
     startDate: new Date(2019, 5, 1),
     endDate: new Date(2019, 8, 31),
     description: [
-      'Benchmarking of the DiViMe speech processing tool.',
-      'Implementation of the automation word count estimation algorithm (Räsänen, Okko, et al., 2019).',
-      'Tags: MATLAB, Python, Docker'
+      <p>Benchmarking of the <u><a href='https://divime.readthedocs.io/en/latest/initial_questions.html#what-is-the-aclew-divime'>DiViMe</a></u> speech processing tool.</p>,
+      <p>Implementation of the <u><a href='https://www.sciencedirect.com/science/article/pii/S0167639318304205'>automatic word count estimation algorithm</a></u> (Räsänen, Okko, et al., 2019).</p>,
+      <p>Tags: <b>MATLAB, Python, Docker</b></p>
     ],
     color: '#ff0000',
     position: 'bottom'
+  },
+  {
+    company: 'EPITA',
+    logo: epita,
+    title: 'Master in Image Processing and Computer Graphics',
+    startDate: new Date(2020, 9, 1),
+    endDate: new Date(2022, 7),
+    description: [
+      <p>Courses in Image Processing, Computer Vision and Computer Graphics.</p>,
+      <p>Tags: <b>C++, Python, OpenGL, CUDA</b></p>
+    ],
+    color: '#0000ff',
+    position: 'center'
   },
   {
     company: 'Factonics',
@@ -77,10 +77,10 @@ const experienceData = [
     startDate: new Date(2020, 9, 1),
     endDate: new Date(2021, 2, 31),
     description: [
-      'Improvement of Factonics\' ML deployment platform.',
-      'Development of a Named-entity recognition program specific to the French health system.',
-      'PoC for a newsletter recommendation system on behalf of BPI France.',
-      'Tags: Python, AWS, Docker'
+      <p>Improvement of Factonics' <b>ML deployment platform</b>.</p>,
+      <p>Development of a <b>Named-entity recognition program</b> specific to the French health system.</p>,
+      <p>PoC for a <b>newsletter recommendation system</b> on behalf of <i>BPI France</i>.</p>,
+      <p>Tags: <b>Python, AWS, Docker</b></p>
     ],
     color: '#ffff00',
     position: 'bottom'
@@ -88,12 +88,13 @@ const experienceData = [
   {
     company: 'Siemens Healthineers',
     logo: siemens,
-    title: 'Software Engineer',
+    title: 'End-of-study project',
     startDate: new Date(2021, 3, 15),
     endDate: new Date(2022, 2, 31),
     description: [
-      'bla bla bla',
-      ''
+      <p>Research of a web based solution for in real-time transparency.</p>,
+      <p>Implementation of <u><a href='https://luebke.us/publications/StochasticTransparency_I3D2010.pdf'>Stochastic Transparency</a></u> (Enderton, Eric, et al., 2010) with ThreeJS.</p>,
+      <p>Tags: <b>Typescript, GLSL, ThreeJS</b></p>
     ],
     color: '#00ff00',
     position: 'bottom'
