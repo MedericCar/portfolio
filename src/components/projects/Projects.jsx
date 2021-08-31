f27966599bb23d7634421b2f11566a95cf863abb
Mederic Carriat <carriatmederic@gmail.com>
2021-08-31 22:31:39 +0200

    Display projects as table

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -3,7 +3,27 @@ import ProjectCard from './ProjectCard'
 import { projectsData } from '../../data'
 import './projects.scss'
 
+const Row = ({ data }) => {
+  return (
+    <div className='row'>
+      {data.map((el) => <ProjectCard data={el}/>)}
+    </div>
+  )
+}
+
 export default function Projects() {
+
+  const chunk = (arr, size) => {
+    let result = []
+    for (let i = 0; i < arr.length; i += size) {
+      let chunk = arr.slice(i, i + size)
+      result.push(chunk)
+    }
+    return result 
+  }
+
+  console.log(chunk(projectsData, 2))
+
   projectsData.sort((a, b) => {
     const yearA = parseInt(a.tags[a.tags.length - 1].text)
     const yearB = parseInt(b.tags[b.tags.length - 1].text)
@@ -19,7 +39,7 @@ export default function Projects() {
         </p>
       </div>
       <div className='cards'>
-        {projectsData.map((el) => <ProjectCard data={el}/>)}
+        {chunk(projectsData, 3).map((el) => <Row data={el}/>)}
       </div>
     </div>
   )
