74f44b91042b77a66babd7662095d5666eb8b5d3
Mederic Carriat <carriatmederic@gmail.com>
2021-09-02 23:36:50 +0200

    Add contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './contact.scss'
 
+
+const renderSendButton = (sent) => {
+  if (sent) {
+    return <button style={{color: 'var(--green)', backgroundColor: 'var(--bg-green)', border: '1px var(--green) solid'}}>Sent !</button>
+  } else {
+    return <button type='submit'>Send</button>
+  }
+}
+
 export default function Contact() {
+
+  const [ sent, setSent ] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setSent(true)
+  }
+
   return (
     <div className='contact' id='contact'>
+    <h1>Contact me</h1>
+    <p>If you have an opportunity for me, feel free to leave a message !</p>
+    <form onSubmit={handleSubmit}>
+      <input type='text' placeholder='Email'/>
+      <textarea placeholder='Message'/>
+      {renderSendButton(sent)}
+    </form>
     </div>
   )
 }
