9b37b3d09a8e6facb421e70c4b6d11f4cefa51b2
Mederic Carriat <carriatmederic@gmail.com>
2021-09-03 09:33:05 +0200

    Add mail sending

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,22 +1,64 @@
 import React, { useState } from 'react'
+import emailjs, { init } from 'emailjs-com'
 import './contact.scss'
 
 
 const renderSendButton = (sent) => {
   if (sent) {
-    return <button style={{color: 'var(--green)', backgroundColor: 'var(--bg-green)', border: '1px var(--green) solid'}}>Sent !</button>
+    return <button disabled style={{cursor:'default', color: 'var(--green)', backgroundColor: 'var(--bg-green)', border: '1px var(--green) solid'}}>Sent !</button>
   } else {
     return <button type='submit'>Send</button>
   }
 }
 
+const isValidEmail = email => {
+  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toLowerCase());
+};
+
+
+
 export default function Contact() {
 
+  const [ name, setName ] = useState('')
+  const [ mail, setMail ] = useState('')
+  const [ message, setMessage ] = useState('')
   const [ sent, setSent ] = useState(false)
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    setSent(true)
+    if (name && mail && message) {
+
+      if (!isValidEmail(mail)) {
+        alert('Please enter a valid email.')
+        return
+      }
+      // TODO - send mail
+      const serviceId = 'service_zc8fbku';
+            const templateId = 'template_3bx52ox';
+            const userId = 'user_9U9sUmHvHS3HJaGwi8WGv';
+            const templateParams = {
+                name,
+                mail,
+                message
+            };
+
+      emailjs
+        .send(serviceId, templateId, templateParams, userId)
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+          }, function(error) {
+            console.log('FAILED...', error);
+          })
+     
+
+      setName('');
+      setMail('');
+      setMessage('');
+      setSent(true);
+    } else {
+      alert('Please fill in all fields.');
+    }
   }
 
   return (
@@ -24,8 +66,9 @@ export default function Contact() {
     <h1>Contact me</h1>
     <p>If you have an opportunity for me, feel free to leave a message !</p>
     <form onSubmit={handleSubmit}>
-      <input type='text' placeholder='Email'/>
-      <textarea placeholder='Message'/>
+      <input type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+      <input type='text' placeholder='Email' value={mail} onChange={(e) => setMail(e.target.value)}/>
+      <textarea placeholder='Message' value={message} onChange={(e) => setMessage(e.target.value)}/>
       {renderSendButton(sent)}
     </form>
     </div>
